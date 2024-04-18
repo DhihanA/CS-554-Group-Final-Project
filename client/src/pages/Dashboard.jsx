8729c681d6d6e12ef42ad9bbc728838e8e162b8e
Jesal Gandhi <jesalgandhi9988@gmail.com>
2024-04-18 00:54:41 -0400

    made accounts compnent for dash

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import BasePage from "./BasePage";
 import Card from "../components/CardComponent";
 import Transactions from "../components/Transactions";
+import AccountCard from "../components/AccountCard";
 
 const Dashboard = () => {
   return (
@@ -11,6 +12,23 @@ const Dashboard = () => {
         <Card />
         <Card /> */}
         <Transactions></Transactions>
+        <div className="p-6 min-h-screen">
+          <h1 className="text-2xl font-bold">Bank accounts</h1>
+          <AccountCard
+            accountType="checkings account"
+            accountNumber="...3137"
+            balance={1053.0}
+            deposits={5005.09}
+            withdrawals={2430.0}
+          />
+          <AccountCard
+            accountType="savings account"
+            accountNumber="...2227"
+            balance={5363.38}
+            deposits={0}
+            withdrawals={0}
+          />
+        </div>
       </div>
     </BasePage>
   );
