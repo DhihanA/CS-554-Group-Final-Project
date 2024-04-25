075cd97433b6d6919b5aa7f3bd795c2b0fd68207
Jesal Gandhi <jesalgandhi9988@gmail.com>
2024-04-25 18:59:06 -0400

    removed nextui ref and fixed dashboard page issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
 import SignUpClerk from "./pages/SignUpPage";
 import LoginClerk from "./pages/LoginPage";
-import {SignedIn, SignedOut} from '@clerk/clerk-react';
-// import { NextUIProvider } from "@nextui-org/react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import "./App.css";
 
 // Import all pages here
@@ -12,33 +11,30 @@ import TransactionsPage from "./pages/TransactionsPage";
 import SettingsPage from "./pages/SettingsPage";
 import LearnPage from "./pages/LearnPage";
 
-
 function App() {
   const navigate = useNavigate();
 
   return (
-    // <NextUIProvider navigate={navigate}>
     <>
       {/* All route declarations go below */}
-      
-        <SignedIn>
-          <Routes>
-            <Route path="/" element={<Dashboard user={{}} />} />
-            <Route path="/transactions" element={<TransactionsPage />} />
-          </Routes>
-        </SignedIn>
 
-        <SignedOut>
-          <Routes>
-            <Route path="/" element={<Dashboard user={{}} />} />
-            <Route path="/login" element={<LoginClerk />}/>
-            <Route path="/signup" element={<SignUpClerk />}/>
-          </Routes>
-        </SignedOut>
+      <SignedIn>
+        <Routes>
+          <Route path="/" element={<DashboardPage user={{}} />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+        </Routes>
+      </SignedIn>
+
+      <SignedOut>
+        <Routes>
+          <Route path="/" element={<DashboardPage user={{}} />} />
+          <Route path="/login" element={<LoginClerk />} />
+          <Route path="/signup" element={<SignUpClerk />} />
+        </Routes>
+      </SignedOut>
 
       {/* Footer Component (todo) here */}
     </>
-    // </NextUIProvider>
   );
 }
 
